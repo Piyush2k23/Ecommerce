9fd59d1ecfb338c3c50f05106c6f38f933ea0f15
Piyush2k23 <piyush23pr2000@gmail.com>
2024-01-10 23:15:04 +0530

    change in routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,14 @@ import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-// import ProductPage from "./pages/ProductPage";
-// import Cart from "./pages/Cart";
+import ProductPage from "./pages/ProductPage";
+import Cart from "./pages/Cart";
 import Home from "./pages/Home";
 import { Provider } from "react-redux";
 import store from "./app/store";
-// import ShoppingInformation from "./pages/ShoppingInformation";
-// import AboutUs from "./pages/AboutUs";
-// import ContactUs from "./pages/ContactUs";
+import ShoppingInformation from "./pages/ShoppingInformation";
+import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
 
 
 function App() {
@@ -22,13 +22,11 @@ function App() {
          <Route path="/" element={<Home />} />
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
-          {/* <Route path="productPage" element={<ProductPage />} />
+          <Route path="productPage" element={<ProductPage />} />
           <Route path="cart" element={<Cart />} />
           <Route path="shopping-information" element={<ShoppingInformation />} />
           <Route path="about" element={<AboutUs />} />
-          <Route path="contact" element={<ContactUs />} /> */}
-
-
+          <Route path="contact" element={<ContactUs />} />
 
            {/* <Route path="contact" element={<Contact />} />
            <Route path="*" element={<NoPage />} />   */}
